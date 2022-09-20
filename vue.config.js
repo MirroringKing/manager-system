365a7d9a05bd19753f85c8581cc5d4752b906713
unknown <xiaoyi>
2022-09-20 20:49:07 +0800

    feat: 跨域

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,29 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 项目打包之后白屏问题
+  publicPath: "./",
   devServer: {
     port: 8888,
     host: "localhost",
     open: true,
     https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: "",
+        },
+      },
+      // "/dev-api": {
+      //   target: "http://localhost:3000",
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     "^/dev-api": "",
+      //   },
+      // },
+    },
   },
   lintOnSave: false,
   productionSourceMap: false,
