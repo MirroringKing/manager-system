c8da316e73124c2ddfb07d461ed2b5a983f5b627
unknown <xiaoyi>
2022-09-20 16:49:09 +0800

    feat: 配置标题、ico图标 serve

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 8888,
+    host: "localhost",
+    open: true,
+    https: false,
+  },
+  lintOnSave: false,
+  productionSourceMap: false,
 });
