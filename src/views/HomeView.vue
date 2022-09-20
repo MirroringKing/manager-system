7787d7386268e6a05d8405494b75b12cafbe2537
unknown <xiaoyi>
2022-09-20 16:59:23 +0800

    feat: 安装了element-ui以及axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button icon="el-icon-search" circle></el-button>
+    <el-button type="primary" icon="el-icon-edit" circle></el-button>
+    <el-button type="success" icon="el-icon-check" circle></el-button>
+    <el-button type="info" icon="el-icon-message" circle></el-button>
+    <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+    <el-button type="danger" icon="el-icon-delete" circle></el-button>
   </div>
 </template>
 
