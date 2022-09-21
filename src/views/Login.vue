9bedd1207c1bbbe1b93e8d2c53516b8a10260a71
unknown <xiaoyi>
2022-09-21 11:57:23 +0800

    feat: 登录页面样式

---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="menu">
+      <h3>会员登录系统</h3>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="账户" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入账户", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "change" },
+        ],
+      },
+    };
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.menu {
+  width: 500px;
+  height: 400px;
+  background-color: skyblue;
+  margin: auto;
+}
+</style>
